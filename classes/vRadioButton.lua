8f2835ce2872bcc28d05c3d37380c78ded3d155b
bjorn-nesby <bjorn.nesby@gmail.com>
2017-12-20 11:18:10 +0100

    initial commit

---
+++ classes/vRadioButton.lua
@@ -0,0 +1,21 @@
+--[[============================================================================
+vRadioButton 
+============================================================================]]--
+--[[
+
+A radio button widget (only one enabled at a time)
+
+]]
+
+--==============================================================================
+
+require (_vlibroot.."vControl")
+
+class 'vRadioButton' (vControl)
+
+function vRadioButton:__init()
+
+  -- TODO
+
+end
+
