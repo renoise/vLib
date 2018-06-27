d02e38120c6b3f2a99e0271732639e127c9319cc
bjorn-nesby <bjorn.nesby@gmail.com>
2018-06-27 16:01:35 +0200

    - vLib: `distribute_sizes()`, method for calculating layout

--- classes/vLib.lua
+++ classes/vLib.lua
@@ -55,7 +55,10 @@ vLib.BITMAP_STYLES = {
 --- (number), standard height for controls
 vLib.CONTROL_H = 18
 
---------------------------------------------------------------------------------
+-- (number), special value that nullifies spacing 
+vLib.NULL_SPACING = -3
+
+---------------------------------------------------------------------------------------------------
 --- generate a unique string that you is used as viewbuilder id for widgets
 -- (avoids clashes in names between multiple instances of the same widget)
 -- @return string, e.g. "vlib12"
@@ -67,7 +70,99 @@ function vLib.generate_uid()
 
 end
 
---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
+-- given a list of fractional sizes, calculate the closest values
+-- e.g. with a target size of 28, {9.33333,9.33333,9.33333} could become {9,9,10} 
+-- * enlarge items that are smaller than the minimum size (subtract from others)
+-- * automatically adjusts items according to the specified amount of spacing  
+-- @param sizes (table<number>)
+-- @param target (number), the "combined" target size 
+-- @param spacing (number), amount of spacing applied to/between items (default to 0)
+-- @param min_width (number), the minimum for a single size (default to 1)
+
+function vLib.distribute_sizes(sizes,target,spacing,min_width)
+  TRACE("vLib.distribute_sizes(sizes,target,spacing,min_width)",sizes,target,spacing,min_width)
+
+  if not spacing then 
+    spacing = 0
+  end
+
+  -- adjust size by the ratio between target size and combined size w. spacing 
+  local combined = 0
+  for k,v in ipairs(sizes) do 
+    combined = combined + v
+  end 
+  local ratio = math.abs(spacing/target)
+  if (spacing < 0) then
+    ratio = target/combined - ratio
+  else 
+    ratio = target/combined + ratio
+  end  
+
+  local min_w = min_width or 1
+  local tmp_fract = 0
+  local enlarged_by = 0
+  local fracts = {}
+  for k,v in ipairs(sizes) do 
+    v = v * ratio
+    local fract = cLib.fraction(v)
+    v = cLib.round_value(v) 
+    -- "carry over" fractional value - apply 
+    -- to item once fraction is higher than 1 
+    if (fract >= 0.5) then 
+      enlarged_by = enlarged_by + (1-fract)
+      tmp_fract = tmp_fract - fract
+    else
+      tmp_fract = tmp_fract + fract
+    end
+    if (tmp_fract > 1) then 
+      tmp_fract = tmp_fract - 1 
+      v = v + 1
+      table.insert(fracts,k)
+    end 
+    v = v - spacing 
+    -- ensure minimum size - and memorize the additional
+    -- pixels we add (subtracted in the next step)
+    if (v < min_w) then 
+      enlarged_by = enlarged_by + (min_w - v)
+      v = min_w
+    end 
+    sizes[k] = v
+  end 
+
+  -- if we have enlarged items, subtract 
+  -- until we reach the same total as before
+  local do_subtract = function(k,v) 
+    if (v > min_w) then 
+      v = v - 1 
+      sizes[k] = v
+      enlarged_by = enlarged_by - 1
+      if (enlarged_by < 1) then 
+        return false
+      end
+    end
+    return true
+  end
+  while (enlarged_by > 1) do
+    -- start with items that got expanded with fractions
+    -- (will ensure a more even-looking distribution)
+    for k,v in ipairs(fracts) do 
+      if not do_subtract(v,sizes[v]) then 
+        break
+      end
+    end
+    for k,v in ipairs(sizes) do
+      if not do_subtract(k,sizes[k]) then 
+        break
+      end
+    end
+  end
+
+  return sizes
+
+end 
+
+---------------------------------------------------------------------------------------------------
 -- retrieve values from the default skin/theme
 
 function vLib.get_skin_color(name)
